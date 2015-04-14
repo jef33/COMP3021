d6ff5159986ac5a38f2a9e910e1f5032f11f528e
cfleungac <cfleungac@CSL4WK42.win2k.cs.ust.hk>
2015-04-14 12:45:18 +0800

    a

--- Blog/src/BlogGUI.java
+++ Blog/src/BlogGUI.java
@@ -21,13 +21,19 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Calendar;
 import java.util.Date;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 
 public class BlogGUI extends JFrame implements ActionListener{
 
+	private JLabel labelWordCount;
 	private JPanel contentPane;
 	private JButton btnPost;
-	private JButton btnRefresh;
 	private JTextArea textAreaPost;
 	private JTextPane textPaneDisplay;
 	
@@ -68,7 +74,7 @@ public class BlogGUI extends JFrame implements ActionListener{
 		splitPane.setLeftComponent(tree);
 		
 		JSplitPane splitPaneRight = new JSplitPane();
-		splitPaneRight.setResizeWeight(0.2);
+		splitPaneRight.setResizeWeight(0.4);
 		splitPaneRight.setOrientation(JSplitPane.VERTICAL_SPLIT);
 		splitPane.setRightComponent(splitPaneRight);
 		
@@ -78,21 +84,39 @@ public class BlogGUI extends JFrame implements ActionListener{
 		splitPaneRight.setLeftComponent(splitPanePost);
 		
 		textAreaPost = new JTextArea();
+		textAreaPost.setLineWrap(true);
+		textAreaPost.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+				int len = textAreaPost.getText().length();
+				if(len <= 140){
+					labelWordCount.setText("You can still input " + (140 - len) + " words.");
+					btnPost.setEnabled(true);
+				}
+				else{
+					labelWordCount.setText("Your post length has exceeded 140 wrods limit.");
+					btnPost.setEnabled(false);
+				}
+			}
+		});
 		splitPanePost.setLeftComponent(textAreaPost);
 		
-		JPanel panel = new JPanel();
-		splitPanePost.setRightComponent(panel);
-		panel.setLayout(new GridLayout(0, 3, 0, 0));
-		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1);
-		
-		btnRefresh = new JButton("Refresh");
-		panel.add(btnRefresh);
+		JSplitPane splitPane_1 = new JSplitPane();
+		splitPane_1.setResizeWeight(0.8);
+		splitPanePost.setRightComponent(splitPane_1);
 		
 		btnPost = new JButton("Post");
-		btnPost.addActionListener(this);
-		panel.add(btnPost);
+		btnPost.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				blog.post(new Post(new Date(), textAreaPost.getText()));
+				textPaneDisplay.setText(blog.list());
+			}
+		});
+		splitPane_1.setRightComponent(btnPost);
+		
+		labelWordCount = new JLabel("You can still input 140 words.");
+		splitPane_1.setLeftComponent(labelWordCount);
 		
 		textPaneDisplay = new JTextPane();
 		textPaneDisplay.setEnabled(true);
@@ -103,12 +127,6 @@ public class BlogGUI extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if(e.getSource() == btnPost){
-			Date d = new Date();
-			blog.post(new Post(d, textAreaPost.getText()));
-			textPaneDisplay.setText(blog.list());
-		}
 	}
 
 }
